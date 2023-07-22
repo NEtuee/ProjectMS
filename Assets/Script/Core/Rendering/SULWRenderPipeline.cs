b7a15b099504191c079d21695e10e03c9a75732f
Ken <odessa5d4@gmail.com>
2023-07-22 22:48:48 +0900

    파이프라인 스크립트 추가

---
+++ Assets/Script/Core/Rendering/SULWRenderPipeline.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SULWRenderTexture2D
+{
+    public SULWRenderTexture2D(string layer, int width, int height, RenderTextureFormat format = RenderTextureFormat.ARGBHalf)
+    {
+        Texture = new RenderTexture(width, height, 1, format);
+        Layer = layer;
+    }
+
+    public SULWRenderTexture2D(string layer, RenderTexture renderTexture)
+    {
+        Texture = renderTexture;
+        Layer = layer;
+    }
+
+    public RenderTexture Texture;
+    public string Layer;
+}
+
+public class SULWRenderPipeline : MonoBehaviour
+{
+
+    // 인덱스가 레이어 번호, 내용물이 레이어 이름
+    // 기본 RGBA16 포맷
+    // 특정 레이어만 다른 포맷 지정하려면 layerFormatSpecifier에 레이어 이름 추가하고 포맷 설정해주면 됨
+    private List<string> layerOrders = new List<string>();
+    
+    [SerializeField] private List<SULWRenderTexture2D> renderTextures = new List<SULWRenderTexture2D>();
+    private Dictionary<string, SULWRenderTexture2D> layerToRenderTexture = new Dictionary<string, SULWRenderTexture2D>();
+    [SerializeField] private Camera internalCamera;
+
+    #region Debug
+    [SerializeField] private List<RenderTexture> _debugRenderTextures;
+    #endregion
+
+    public const int RENDERING_LAYER_FROM = 6;
+    public const int RENDERING_LAYER_TO = 18;
+
+    private void Awake()
+    {
+        int debugCount = 0;
+        for (int i = RENDERING_LAYER_FROM; i < RENDERING_LAYER_TO; ++i)
+        {
+            string engineLayer = LayerMask.LayerToName(i);
+
+            if (engineLayer == string.Empty)
+            {
+                continue;
+            }
+
+            if (_debugRenderTextures.Count > debugCount)
+            {
+                var renderTexture = new SULWRenderTexture2D(engineLayer, _debugRenderTextures[debugCount]);
+                layerOrders.Add(engineLayer);
+                renderTextures.Add(renderTexture);
+                layerToRenderTexture.Add(engineLayer, renderTexture);
+                debugCount++;
+            }
+            else
+            {
+                if (debugCount != 0)
+                {
+                    return;
+                }
+                var renderTexture = new SULWRenderTexture2D(engineLayer, Screen.width, Screen.height);
+                layerOrders.Add(engineLayer);
+                renderTextures.Add(renderTexture);
+                layerToRenderTexture.Add(engineLayer, renderTexture);
+            }
+
+        }
+    }
+    private void LateUpdate()
+    {
+        internalDraw();
+    }
+
+    private void internalDraw()
+    {
+        for (int i = 0; i < layerOrders.Count; ++i)
+        {
+            string keyLayer = layerOrders[i];
+            RenderTexture renderTexture = layerToRenderTexture[keyLayer].Texture;
+            //Graphics.SetRenderTarget(layerToRenderTexture[keyLayer].Texture);
+
+            internalCamera.cullingMask |= 1 << LayerMask.NameToLayer(layerOrders[i]);
+
+            internalCamera.targetTexture = renderTexture;
+            internalCamera.Render();
+
+            internalCamera.cullingMask = 0;
+        }
+    }
+}
