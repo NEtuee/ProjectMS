d693b6a98a44fb239672b75906873c2cb9251b34
Umin Lee <netuee@naver.com>
2024-01-28 02:52:51 +0900

    dash point delay

--- Assets/Script/GameScript/GameUI/DashPointUI.cs
+++ Assets/Script/GameScript/GameUI/DashPointUI.cs
@@ -9,7 +9,8 @@ public class DashPointUI : IUIElement
 
     private bool[] _dashPointGen;
     private DashPointIcon[] _dashPointIcon;
-    private float _prevBlood;
+    private float _prevBlood = 0f;
+    private float _prevDashPoint = 0f;
 
     public bool CheckValidBinderLink(out string reason)
     {
@@ -81,29 +82,31 @@ public class DashPointUI : IUIElement
         _absorbingFlag = _absorbingFlag == false ? _prevBlood < blood : _absorbingFlag;
         _prevBlood = blood;
 
+        float prevDashPointIndex = MathEx.floor(_prevDashPoint, 0);
         bool releaseAbsorbing = false;
         for (int i = 0; i < _dashPointGen.Length; i++)
         {
             float current = (float)i + 1;
-            
+
             var currentState = dashPoint >= current;
             if (currentState != _dashPointGen[i])
             {
                 if (currentState == true)
                 {
+                    float delayTime = (current - prevDashPointIndex) * 0.1f;
                     if (_absorbingFlag == true)
                     {
                         releaseAbsorbing = true;
-                        _dashPointIcon[i].Absorbing();
+                        _dashPointIcon[i].ChangeIconState(DashPointIconState.Absorbing,delayTime);
                     }
                     else
                     {
-                        _dashPointIcon[i].AutoCharging();
+                        _dashPointIcon[i].ChangeIconState(DashPointIconState.AutoCharging,delayTime);
                     }
                 }
                 else
                 {
-                    _dashPointIcon[i].Breaking();
+                    _dashPointIcon[i].ChangeIconState(DashPointIconState.Breaking,0f);
                 }
             }
             _dashPointGen[i] = currentState;
@@ -115,6 +118,8 @@ public class DashPointUI : IUIElement
         {
             _absorbingFlag = false;
         }
+
+        _prevDashPoint = dashPoint;
     }
     
     private struct AnimationPresetInfo
@@ -129,13 +134,25 @@ public class DashPointUI : IUIElement
         }
     }
     
-        private class DashPointIcon
+    private enum DashPointIconState
+    {
+        None,
+        Absorbing,
+        AutoCharging,
+        Breaking,
+    }
+
+    private class DashPointIcon
     {
         private Image _image;
         private AnimationPlayer _animationPlayer;
         private AnimationPresetInfo _absorbingAnimPreset;
         private AnimationPresetInfo _autoChargingPreset;
         private AnimationPresetInfo _breakingPreset;
+
+        private DashPointIconState _iconState = DashPointIconState.None;
+        private bool _iconStateChanged = false;
+        private float _iconStateDelay = 0f;
 
         public DashPointIcon(Image ownerImage, string absorbingAniPath, string autoChargingAniPath, string breakingAniPath)
         {
@@ -144,13 +161,55 @@ public class DashPointUI : IUIElement
             _absorbingAnimPreset = new AnimationPresetInfo(ResourceContainerEx.Instance().GetAnimationCustomPreset($"{absorbingAniPath}/"), absorbingAniPath);
             _autoChargingPreset = new AnimationPresetInfo(ResourceContainerEx.Instance().GetAnimationCustomPreset($"{autoChargingAniPath}/"), autoChargingAniPath);
             _breakingPreset = new AnimationPresetInfo(ResourceContainerEx.Instance().GetAnimationCustomPreset($"{breakingAniPath}/"), breakingAniPath);
+
+            _iconState = DashPointIconState.None;
+            _iconStateChanged = false;
+            _iconStateDelay = 0f;
         }
         
         public void Progress(float deltaTime)
         {
+            UpdateIconState(deltaTime);
+
             _animationPlayer.progress(deltaTime, null);
             _image.sprite = _animationPlayer.getCurrentSprite();
             _image.SetNativeSize();
+        }
+
+        private void UpdateIconState(float deltaTime)
+        {
+            if(_iconStateChanged == false)
+                return;
+
+            _iconStateDelay -= deltaTime;
+            if(_iconStateDelay > 0f)
+                return;
+            
+            _iconStateDelay = 0f;
+            switch(_iconState)
+            {
+                case DashPointIconState.None:
+                break;
+                case DashPointIconState.Absorbing:
+                    Absorbing();
+                break;
+                case DashPointIconState.AutoCharging:
+                    AutoCharging();
+                break;
+                case DashPointIconState.Breaking:
+                    Breaking();
+                break;
+            }
+
+            _iconStateChanged = false;
+        }
+
+        public void ChangeIconState(DashPointIconState iconState, float delay)
+        {
+            _iconState = iconState;
+            _iconStateDelay = delay;
+
+            _iconStateChanged = true;
         }
 
         public void Absorbing()
