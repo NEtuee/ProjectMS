0919866513bee1f08c599fc110959e3760960545
Umin Lee <netuee@naver.com>
2024-02-28 02:37:52 +0900

    wip

--- Assets/Script/GameScript/BackgroundLayer.cs
+++ Assets/Script/GameScript/BackgroundLayer.cs
@@ -7,6 +7,9 @@ public class BackgroundLayer : MonoBehaviour
 {
     public float _movementRate = 1f;
     public bool _skybox = false;
+
+    public bool _lockX = false;
+    public bool _lockY = false;
     
     void Update()
     {
@@ -17,12 +20,24 @@ public class BackgroundLayer : MonoBehaviour
         {
             Vector3 position = Camera.main.transform.position;
             position.z = 0f;
-            transform.position = position + position * -_movementRate;
+            
+            Vector3 resultPosition = position + position * -_movementRate;
+            if(_lockX)
+                resultPosition.x = position.x;
+            if(_lockY)
+                resultPosition.y = position.y;
+
+            transform.position = resultPosition;
         }
         else
         {
             Vector3 position = Camera.main.transform.position * -_movementRate;
             position.z = 0f;
+
+            if(_lockX)
+                position.x = 0f;
+            if(_lockY)
+                position.y = 0f;
 
             transform.position = position;
         }
