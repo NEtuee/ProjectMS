e88bf9bd073d15966ac730520ade640de504a903
Umin Lee <netuee@naver.com>
2024-09-18 00:22:28 +0900

    sequencer macro

--- Assets/Script/Core/FileIO/SequencerGraphExporter.cs
+++ Assets/Script/Core/FileIO/SequencerGraphExporter.cs
@@ -132,6 +132,40 @@ public class SequencrGraphLoader : LoaderBase<SequencerGraphBaseData>
         return readSequencerGraphData(xmlDoc.FirstChild);
     }
 
+    public static List<XmlNode> readMacro(string path, ref List<XmlNode> nodes)
+    {
+        _currentFileName = path;
+        
+        PositionXmlDocument xmlDoc = new PositionXmlDocument();
+        try
+        {
+            XmlReaderSettings readerSettings = new XmlReaderSettings();
+            readerSettings.IgnoreComments = true;
+            using (XmlReader reader = XmlReader.Create(XMLScriptConverter.convertXMLScriptSymbol(_currentFileName),readerSettings))
+            {
+                xmlDoc.Load(reader);
+            }
+        }
+        catch(System.Exception ex)
+        {
+            DebugUtil.assert(false,"xml load exception : {0} \n{1}",path,ex.Message);
+            return null;
+        }
+        
+        if(xmlDoc.HasChildNodes == false)
+        {
+            DebugUtil.assert(false,"xml is empty");
+            return null;
+        }
+
+        for(int i = 0; i < xmlDoc.FirstChild.ChildNodes.Count; ++i)
+        {
+            nodes.Add(xmlDoc.FirstChild.ChildNodes[i]);
+        }
+
+        return nodes;
+    }
+
     public static SequencerGraphBaseData readSequencerGraphData(XmlNode node)
     {
         SequencerGraphBaseData baseData = new SequencerGraphBaseData();
@@ -148,11 +182,28 @@ public class SequencrGraphLoader : LoaderBase<SequencerGraphBaseData>
             }
         }
 
+        List<XmlNode> macroNodes = new List<XmlNode>();
+
         XmlNodeList nodes = node.ChildNodes;
         for(int nodeIndex = 0; nodeIndex < nodes.Count; ++nodeIndex)
         {
             XmlNode phaseNode = nodes[nodeIndex];
-            SequencerGraphPhaseData phaseData = readPhaseData(phaseNode);
+
+            if(phaseNode.Name == "Include")
+            {
+                for(int i = 0; i < phaseNode.Attributes.Count; ++i)
+                {
+                    if(phaseNode.Attributes[i].Name == "Path")
+                    {
+                        readMacro(phaseNode.Attributes[i].Value, ref macroNodes);
+                        break;
+                    }
+                }
+
+                continue;
+            }
+
+            SequencerGraphPhaseData phaseData = readPhaseData(phaseNode, ref macroNodes);
 
             if(phaseNode.Name == "InitializePhase")
                 baseData._sequencerGraphPhase[(int)SequencerGraphPhaseType.Initialize] = phaseData;
@@ -160,6 +211,7 @@ public class SequencrGraphLoader : LoaderBase<SequencerGraphBaseData>
                 baseData._sequencerGraphPhase[(int)SequencerGraphPhaseType.Update] = phaseData;
             else if(phaseNode.Name == "EndPhase")
                 baseData._sequencerGraphPhase[(int)SequencerGraphPhaseType.End] = phaseData;
+            
         }
 
         for(int index = 0; index < (int)SequencerGraphPhaseType.Count; ++index)
@@ -171,7 +223,7 @@ public class SequencrGraphLoader : LoaderBase<SequencerGraphBaseData>
         return baseData;
     }
 
-    private static SequencerGraphPhaseData readPhaseData(XmlNode node)
+    private static SequencerGraphPhaseData readPhaseData(XmlNode node, ref List<XmlNode> macroNodes)
     {
         SequencerGraphPhaseData phaseData = new SequencerGraphPhaseData();
 
@@ -179,6 +231,35 @@ public class SequencrGraphLoader : LoaderBase<SequencerGraphBaseData>
         XmlNodeList eventNodes = node.ChildNodes;
         for(int i = 0; i < eventNodes.Count; ++i)
         {
+            if(eventNodes[i].Name == "UseMacro")
+            {
+                for(int x = 0; x < eventNodes[i].Attributes.Count; ++x)
+                {
+                    if(eventNodes[i].Attributes[x].Name != "Name")
+                        continue;
+
+                    string value = eventNodes[i].Attributes[x].Value;
+                    foreach(var item in macroNodes)
+                    {
+                        if(item.Name != value)
+                            continue;
+
+                        for(int index = 0; index < item.ChildNodes.Count; ++index)
+                        {
+                            SequencerGraphEventBase macroEventData = readEventData(item.ChildNodes[index]);
+                            eventList.Add(macroEventData);
+
+                        }
+                        
+                        break;
+                    }
+
+                    break;
+                }
+
+                continue;
+            }
+
             SequencerGraphEventBase eventData = readEventData(eventNodes[i]);
             eventList.Add(eventData);
         }
