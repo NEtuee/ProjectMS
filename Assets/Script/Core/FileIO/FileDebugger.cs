7e1bffc42c21a9afb0419c854b0d817e06e3e617
UminLee <netuee@naver.com>
2024-11-21 21:40:03 +0900

    mac support

--- Assets/Script/Core/FileIO/FileDebugger.cs
+++ Assets/Script/Core/FileIO/FileDebugger.cs
@@ -9,10 +9,35 @@ public class FileDebugger
 {
     public static void OpenFileWithCursor(string filePath, int lineNumber)
     {
-        string vscodePath = "code";
-        string command = $"{vscodePath} -g {filePath}:{lineNumber}";
+        if(Application.platform == RuntimePlatform.OSXEditor)
+		{
+            // VS Code 실행 파일 경로 (Mac 기준)
+            string vscodePath = "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code";
 
-        Process.Start("cmd.exe", $"/c {command}");
+            // 프로세스를 실행하여 VS Code로 파일 열기
+            Process process = new Process();
+            process.StartInfo.FileName = vscodePath;
+            process.StartInfo.Arguments = $"--goto \"{filePath}:{lineNumber}\""; // 파일 경로를 인자로 전달
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            try
+            {
+                process.Start();
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogError($"Failed to open file in VS Code: {ex.Message}");
+            }
+        }
+        else
+        {
+            string vscodePath = "code";
+            string command = $"{vscodePath} -g {filePath}:{lineNumber}";
+
+            Process.Start("cmd.exe", $"/c {command}");
+        }
     }
 
     public static int findLine(string filePath, string targetString)
